06115e2e65b955bf1711616d46e7415c9197a4bd
Antonio <vuminhtuan3795@gmail.com>
2024-03-05 05:00:14 +0700

    refactor: adding relative absolute import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  // base: './'
+  resolve: {
+    // alias: [
+    //   { find:'~', replacement: './src' }
+    // ]
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components')
+    }
+  }
+})
